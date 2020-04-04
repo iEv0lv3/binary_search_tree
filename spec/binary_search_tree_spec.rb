16c97c2cda34836743b4fd3380f54e47ec8c7abd
Jordan <jordan.wallace.williams@gmail.com>
2020-04-03 21:22:01 -0600

    Update spec to work with tree and node refactor, add test for searching

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -5,7 +5,15 @@ require './lib/node'
 
 describe 'Binary search tree' do
   before :each do
-    @tree = Tree.new(50)
+    @tree = Tree.new(Node.new(50))
+    @nodes = []
+    numbers = (1...100).to_a.shuffle!
+    numbers.delete(@tree.root.data)
+    50.times { @nodes.push(Node.new(numbers.pop)) }
+
+    @nodes.each do |node|
+      @tree.insert(node)
+    end
   end
 
   it 'tree root node value can be set' do
@@ -13,13 +21,29 @@ describe 'Binary search tree' do
   end
 
   it 'nodes can be inserted into a tree' do
-    nodes = []
-    numbers = (1...100).to_a.shuffle!
-    numbers.delete(@tree.root.data)
-    99.times { nodes.push(Node.new(numbers.pop)) }
+    tree = Tree.new(Node.new(5))
+    node1 = tree.insert(Node.new(3))
+    expect(tree.root.left_branch).to eq(node1)
 
-    nodes.each do |node|
-      @tree.insert(node)
-    end
+    node2 = tree.insert(Node.new(4))
+    expect(tree.root.left_branch.right_branch).to eq(node2)
+
+    node3 = tree.insert(Node.new(7))
+    expect(tree.root.right_branch).to eq(node3)
+
+    node4 = tree.insert(Node.new(6))
+    expect(tree.root.right_branch.left_branch).to eq(node4)
+  end
+
+  it 'tree can be searched' do
+    tree = Tree.new(Node.new(5))
+    tree.insert(Node.new(3))
+    tree.insert(Node.new(4))
+    tree.insert(Node.new(7))
+    node1 = tree.insert(Node.new(6))
+
+    expect(tree.include?(17)).to eq(false)
+    expect(tree.include?(6)).to eq({ 'true' => node1 })
+    expect(tree.include?(5)).to eq({ 'true' => tree.root })
   end
 end
