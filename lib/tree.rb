058b5b44d2bef896d8f7ac5b2a910b5b6bf7bb47
Jordan <jordan.wallace.williams@gmail.com>
2020-04-03 21:22:45 -0600

    Refactor .include? to be more elegant

--- lib/tree.rb
+++ lib/tree.rb
@@ -3,28 +3,24 @@ require_relative 'node'
 class Tree
   attr_reader :root
 
-  def initialize(root)
-    @root = Node.new(root)
+  def initialize(data = nil)
+    @root = data
   end
 
   def insert(node)
+    @root = node if @root.nil?
     @root.node_step(node, @root)
   end
 
   def include?(search, node = @root)
-    confirm_search(search)
-    return nil if node.nil?
+    return false if node.nil?
 
     if search < node.data
       include?(search, node.left_branch)
     elsif search > node.data
       include?(search, node.right_branch)
     else
-      node
+      { 'true' => node }
     end
-  end
-
-  def confirm_search(search)
-    puts 'Please enter an integer' if search != Integer
   end
 end
